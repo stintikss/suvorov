66a289b0968c4c1cfa2d4f6068336b338121fe0a
Egor <tumkasuv811@gmail.com>
2025-06-17 22:35:01 +0500

    Update router.tsx

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/home';
-import NotFound from '../pages/NotFound';
+// import NotFound from '../pages/NotFound';
 
 const Router: React.FC = () => {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       
-      {/* <Route path="*" element={<Navigate to="/about" replace />} /> */}
-      <Route path='*' element={<NotFound />} />
+      <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   );
 };
