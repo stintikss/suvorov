ad64511acec2001e4d8ee405ce7477fbdf19e977
Egor <tumkasuv811@gmail.com>
2025-06-21 02:02:29 +0500

    2.;6

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
-// import SplashScreenHelper from './tools/SplashScreenHelper'
-import Router from './router/router'
+import Routers from './router/router'
 // import Coockie from './tools/cookie_accept/cookie'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Router />
+    <Router>
+      <Routers />
+      {/* <Coockie /> */}
+    </Router>
   </StrictMode>,
 )
